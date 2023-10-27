cf82d32aab917c1bc356e40f8ed3a3eaa2b47e93
starkeOnline <starke.robin@outlook.com>
2023-10-27 17:24:50 +0200

    Fixed JSON fetching by using a for loop to get all the post from dummyJSON, now working as intended. Minor CSS changes.

--- JSReddit.js
+++ JSReddit.js
@@ -1,17 +1,26 @@
-
-fetch('https://dummyjson.com/posts/1')
+fetch('https://dummyjson.com/posts/')
 .then(res => res.json())
-.then((postone) => { 
+.then((posts) => { 
   
-let first = document.getElementById("title");
-first.innerHTML = JSON.stringify(postone.title).replaceAll('"', "");
-document.body.append(first);
+  let pozt = posts;
 
-let second = document.getElementById("tags");
-second.innerHTML = JSON.stringify(postone.tags).replaceAll("[", "").replace("]", "").replaceAll('"', "");
-document.body.append(second);
-  
-let third = document.getElementById("bod");
-third.innerHTML = JSON.stringify(postone.body).replaceAll('"', "");
-document.body.append(third);
-});
+  for (let i = 0; i < pozt.posts.length; i++) {
+    let divs = document.createElement("div");
+
+    let postTitle = pozt.posts[i].title;
+    divs.innerHTML = "<h1>" + postTitle + "</h1>";
+    
+    let postTag = pozt.posts[i].tags;
+    divs.innerHTML += "<h2>" + postTag + "</h2>";
+
+    let postBody = pozt.posts[i].body;
+    divs.innerHTML += "<h3>" + postBody + "</h3>";
+
+    document.body.append(divs);
+    divs.classList.add("posted");
+}
+
+
+}
+);
+
