d9598407e2f68c21bf73887d15bfe27f63d0a2c0
starkeOnline <starke.robin@outlook.com>
2023-10-30 16:31:13 +0100

    Minor CSS changes and tests regarding input order.

--- JSReddit.js
+++ JSReddit.js
@@ -6,30 +6,31 @@ fetch('https://dummyjson.com/posts/')
   let post = posts;
 
     for (let i = 0; i < post.posts.length; i++) {
-      let divs = document.createElement("div");
+      let divPost = document.createElement("div");
 
       //Hämtar och printar titeln
       let postTitle = post.posts[i].title;
-      divs.innerHTML = "<h1>" + postTitle + "</h1>";
+      divPost.innerHTML = "<h1>" + postTitle + "</h1>";
       
       //Hämtar och printar första tagen
       let postTag = post.posts[i].tags;
       let tagOne = postTag.slice(0, 1);
-      divs.innerHTML += "<h2>" + tagOne + "</h2>";
+      divPost.innerHTML += "<h2>" + tagOne + "</h2>";
 
       //Hämtar och printar andra tagen
       let tagTwo = postTag.slice(1, 2);
-      divs.innerHTML += "<h2>" + tagTwo + "</h2>";
+      divPost.innerHTML += "<h2>" + tagTwo + "</h2>";
 
       //Hämtar och printar ev. tredje tagen
       let tagThree = postTag.slice(2, 3);
-      divs.innerHTML += "<h2>" + tagThree + "</h2>";
+      divPost.innerHTML += "<h2>" + tagThree + "</h2>";
 
+      //Hämtar och printar inlägget
       let postBody = post.posts[i].body;
-      divs.innerHTML += "<h3>" + postBody + "</h3>";
+      divPost.innerHTML += "<h3>" + postBody + "</h3>";
 
-      document.body.append(divs);
-      divs.classList.add("posted");     
+      document.body.append(divPost);
+      divPost.classList.add("oldPosts");     
     }
   } 
 );
@@ -56,21 +57,21 @@ for (let i = 0; i < collapse.length; i++) {
 // Function for getting input from the "create post"-fields and posting on the page after clicking on "post"-button
 function onClick() {
   event.preventDefault();
-  let divs = document.createElement("div");
+  let divPost = document.createElement("div");
   let postNewTitle = document.getElementById("inputTitle").value;
   let postNewTagOne = document.getElementById("inputTagOne").value;
   let postNewTagTwo = document.getElementById("inputTagTwo").value;
   let postNewTagThree = document.getElementById("inputTagThree").value;
   let postNewPost = document.getElementById("inputPost").value;
 
-  divs.innerHTML += "<h1>" + postNewTitle + "</h1>";
-  divs.innerHTML += "<h2>" + postNewTagOne + "</h2>";
-  divs.innerHTML += "<h2>" + postNewTagTwo + "</h2>";
-  divs.innerHTML += "<h2>" + postNewTagThree + "</h2>";
-  divs.innerHTML += "<h3>" + postNewPost + "</h3>";
+  divPost.innerHTML += "<h1>" + postNewTitle + "</h1>";
+  divPost.innerHTML += "<h2>" + postNewTagOne + "</h2>";
+  divPost.innerHTML += "<h2>" + postNewTagTwo + "</h2>";
+  divPost.innerHTML += "<h2>" + postNewTagThree + "</h2>";
+  divPost.innerHTML += "<h3>" + postNewPost + "</h3>";
 
-  document.body.append(divs);
-  divs.classList.add("posted");
+  document.body.append(divPost);
+  divPost.classList.add("newPosts");
  
   //Clearing fields after getting the values
   document.getElementById("inputTitle").value = "";
@@ -78,6 +79,7 @@ function onClick() {
   document.getElementById("inputTagTwo").value = "";
   document.getElementById("inputTagThree").value = "";
   document.getElementById("inputPost").value = "";
+
 }
 postButton.addEventListener("click", onClick);
 
