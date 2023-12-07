26f3c820dedb8b0fa23e5f4852a9f69d0432db5b
starkeOnline <starke.robin@outlook.com>
2023-12-07 15:37:01 +0100

    Eventlisteners added to buttons, upvoting and downvoting now working as intended

--- JSReddit.js
+++ JSReddit.js
@@ -5,7 +5,7 @@ if (checkStorage == null) {
 
 
   // Fetching posts from API
-  fetch('https://dummyjson.com/posts/')
+  fetch("https://dummyjson.com/posts/")
     .then(res => res.json())
     .then((posts) => {
 
@@ -14,6 +14,8 @@ if (checkStorage == null) {
       for (let i = 0; i < post.posts.length; i++) {
         let divPost = document.createElement("div");
         let votePost = document.createElement("div");
+        let upVote = document.createElement("button");
+        let downVote = document.createElement("button");
 
         // Printing title
         let postTitle = post.posts[i].title;
@@ -40,33 +42,33 @@ if (checkStorage == null) {
 
         // Printing number of likes
         let postLikes = post.posts[i].reactions;
-        votePost.innerHTML += "<h6>" + postLikes + "</h6>" + "<button>↑</button>" + "<button>↓</button>";
+        votePost.innerHTML += "<h6>" + postLikes + "</h6>";
 
+        upVote.innerHTML += "↑";
+        downVote.innerHTML += "↓";
 
+        // Upvoting and downvoting eventlisteners
+        upVote.addEventListener("click", function () {
+          postLikes++;
+          votePost.innerHTML = "<h6>" + postLikes + "</h6>"
+        });
 
-        // Adding classes to the like button and dislike button
-        let btns = document.getElementsByTagName("button")
+        downVote.addEventListener("click", function () {
+          postLikes--;
+          votePost.innerHTML = "<h6>" + postLikes + "</h6>"
+        });
 
-        for (let i = 0; i < btns.length; i++) {
-          
-          if (btns[i].textContent === "↑") {
-            btns[i].classList.add("likeButton");
-            btns[i].addEventListener("click", function(){
-              
-          })
-          
-        }
-          if (btns[i].textContent === "↓") {
-            btns[i].classList.add("dislikeButton");
-          }
-          
-        }
+        // Appending the divs with new information
+        document.getElementById("newPostsDiv").appendChild(divPost);
+        document.getElementById("newPostsDiv").appendChild(votePost);
+        document.getElementById("newPostsDiv").appendChild(upVote);
+        document.getElementById("newPostsDiv").appendChild(downVote);
 
-
-        document.getElementById("oldPostsDiv").appendChild(divPost);
-        document.getElementById("oldPostsDiv").appendChild(votePost);
         divPost.classList.add("oldPosts");
         votePost.classList.add("oldVotePosts");
+        upVote.classList.add("likeButton");
+        downVote.classList.add("dislikeButton");
+
 
 
         // Adding the fetched posts to local storage
@@ -114,6 +116,9 @@ else {
 
         let divPost = document.createElement("div");
         let votePost = document.createElement("div");
+        let upVote = document.createElement("button");
+        let downVote = document.createElement("button");
+
 
         let postNewTitle = values[0];
         let postNewTagOne = values[1];
@@ -129,50 +134,33 @@ else {
         divPost.innerHTML += "<br>"
         divPost.innerHTML += "<br>"
         divPost.innerHTML += "<h3>" + postNewPost + "</h3>";
-    
-        votePost.innerHTML += "<h6>" + postNewLikes + "</h6>" + "<button>↑</button>" + "<button>↓</button>";
 
-        // Adding class for styling in CSS
-        divPost.classList.add("newPosts");
-        votePost.classList.add("newVotePosts");
+        votePost.innerHTML += "<h6>" + postNewLikes + "</h6>";
 
-        // Adding classes to the like button and dislike button
-        let btns = document.getElementsByTagName("button")
-        for (let i = 0; i < btns.length; i++) {
-          if (btns[i].textContent === "↑") {
+        upVote.innerHTML += "↑";
+        downVote.innerHTML += "↓";
 
-            btns[i].classList.add("likeButton");
-            btns[i].addEventListener("click", function(){
+        // Upvoting and downvoting eventlisteners
+        upVote.addEventListener("click", function () {
+          postNewLikes++;
+          votePost.innerHTML = "<h6>" + postNewLikes + "</h6>"
+        });
 
+        downVote.addEventListener("click", function () {
+          postNewLikes--;
+          votePost.innerHTML = "<h6>" + postNewLikes + "</h6>"
+        });
 
-              //let up = postNewLikes;
-
-              let up = btns[i];
-
-
-              
-              
-
-              console.log(up)
-
-              //document.getElementById("newPostsDiv").appendChild(votePost);
-              
-            })
-            
-        
-        
-        
-        }
-
-
-          if (btns[i].textContent === "↓") {
-            btns[i].classList.add("dislikeButton");
-          }
-        }
-
-
+        // Appending the divs with new information
         document.getElementById("newPostsDiv").appendChild(divPost);
         document.getElementById("newPostsDiv").appendChild(votePost);
+        document.getElementById("newPostsDiv").appendChild(upVote);
+        document.getElementById("newPostsDiv").appendChild(downVote);
+
+        divPost.classList.add("oldPosts");
+        votePost.classList.add("oldVotePosts");
+        upVote.classList.add("likeButton");
+        downVote.classList.add("dislikeButton");
       }
     }
   }
@@ -197,124 +185,85 @@ for (let i = 0; i < collapse.length; i++) {
 
 
 
-  // Function for getting input from the "create post"-fields and posting on the page after clicking on "post"-button
-  function onClick() {
-    event.preventDefault();
-    let divPost = document.createElement("div");
-    let votePost = document.createElement("div");
+// Function for getting input from the "create post"-fields and posting on the page after clicking on "post"-button
+function onClick() {
+  event.preventDefault();
+  let divPost = document.createElement("div");
+  let votePost = document.createElement("div");
+  let upVote = document.createElement("button");
+  let downVote = document.createElement("button");
 
-    let postNewTitle = document.getElementById("inputTitle").value;
-    let postNewTagOne = document.getElementById("inputTagOne").value;
-    let postNewTagTwo = document.getElementById("inputTagTwo").value;
-    let postNewTagThree = document.getElementById("inputTagThree").value;
-    let postNewPost = document.getElementById("inputPost").value;
-    let postNewLikes =  0;
+  let postNewTitle = document.getElementById("inputTitle").value;
+  let postNewTagOne = document.getElementById("inputTagOne").value;
+  let postNewTagTwo = document.getElementById("inputTagTwo").value;
+  let postNewTagThree = document.getElementById("inputTagThree").value;
+  let postNewPost = document.getElementById("inputPost").value;
+  let postNewLikes = 0;
 
-    // If title and post input fields are empty, nothing is posted.
-    if (inputTitle.value == "" || inputPost.value == "") {
-      alert("Title and post cannot be empty");
-    }
-    else {
-      divPost.innerHTML += "<h1>" + postNewTitle + "</h1>";
-      divPost.innerHTML += "<h2>" + postNewTagOne + "</h2>";
-      divPost.innerHTML += "<h2>" + postNewTagTwo + "</h2>";
-      divPost.innerHTML += "<h2>" + postNewTagThree + "</h2>";
-      divPost.innerHTML += "<br>"
-      divPost.innerHTML += "<br>"
-      divPost.innerHTML += "<h3>" + postNewPost + "</h3>";
-
-      votePost.innerHTML += "<h6>" + postNewLikes + "</h6>" + "<button>↑</button>" + "<button>↓</button>";
-
-
-      let newTags = document.getElementsByTagName("h2");
-      for (let i = 0; i < newTags.length; i++) {
-        newTags[i].classList.add("tags");
-      }
-
-      // Adding class for styling in CSS
-      divPost.classList.add("newPosts");
-      votePost.classList.add("newVotePosts");
-
-      document.getElementById("newPostsDiv").appendChild(divPost);
-      document.getElementById("newPostsDiv").appendChild(votePost);
-    }
-
-    // Inserting new posts above the old ones
-    let previousPosts = document.getElementById("newPostsDiv");
-    previousPosts.insertBefore(divPost, votePost, previousPosts.firstChild);
-
-    // Clearing fields after getting the values
-    document.getElementById("inputTitle").value = "";
-    document.getElementById("inputTagOne").value = "";
-    document.getElementById("inputTagTwo").value = "";
-    document.getElementById("inputTagThree").value = "";
-    document.getElementById("inputPost").value = "";
-    
-
-    // Adding classes to the like button and dislike button
-    let btns = document.getElementsByTagName("button")
-    for (let i = 0; i < btns.length; i++) {
-      if (btns[i].textContent === "↑") {
-        btns[i].classList.add("likeButton");
-        btns[i].addEventListener("click", function(){
-              
-          for (let i = 0; i < btns[i].length; i++) {
-            console.log(btns[i]);
-          }
-      })
-    }
-      if (btns[i].textContent === "↓") {
-        btns[i].classList.add("dislikeButton");
-      }
-    }
-
-
-    // Adding new posts to local storage
-    let checkStorage = localStorage.getItem("storingValues");
-
-    if (checkStorage != null) {
-
-      let storeValues = [postNewTitle, postNewTagOne, postNewTagTwo, postNewTagThree, postNewPost, postNewLikes]
-
-      let storingPosts = JSON.parse(localStorage.getItem("storingValues"));
-      storingPosts.unshift(storeValues);
-      localStorage.setItem("storingValues", JSON.stringify(storingPosts));
-    }
-
-    else {
-
-      let storeNewValues = [postNewTitle, postNewTagOne, postNewTagTwo, postNewTagThree, postNewPost, postNewLikes]
-      let storingPosts = [storeNewValues];
-      localStorage.setItem("storingValues", JSON.stringify(storingPosts));
-    }
-
+  // If title and post input fields are empty, nothing is posted.
+  if (inputTitle.value == "" || inputPost.value == "") {
+    alert("Title and post cannot be empty");
   }
-  postButton.addEventListener("click", onClick);
+  else {
+    divPost.innerHTML += "<h1>" + postNewTitle + "</h1>";
+    divPost.innerHTML += "<h2>" + postNewTagOne + "</h2>";
+    divPost.innerHTML += "<h2>" + postNewTagTwo + "</h2>";
+    divPost.innerHTML += "<h2>" + postNewTagThree + "</h2>";
+    divPost.innerHTML += "<br>"
+    divPost.innerHTML += "<br>"
+    divPost.innerHTML += "<h3>" + postNewPost + "</h3>";
+
+    votePost.innerHTML += "<h6>" + postNewLikes + "</h6>";
 
 
-  /*
-  let up = document.getElementsByClassName("likeButton");
-  for (var i = 0; i < items.length; i++) {
-    up[i].addEventListener("click", upVote);
+    let newTags = document.getElementsByTagName("h2");
+    for (let i = 0; i < newTags.length; i++) {
+      newTags[i].classList.add("tags");
+    }
 
+    // Adding class for styling in CSS
+    document.getElementById("newPostsDiv").appendChild(divPost);
+    document.getElementById("newPostsDiv").appendChild(votePost);
+    document.getElementById("newPostsDiv").appendChild(upVote);
+    document.getElementById("newPostsDiv").appendChild(downVote);
+
+    divPost.classList.add("oldPosts");
+    votePost.classList.add("oldVotePosts");
+    upVote.classList.add("likeButton");
+    downVote.classList.add("dislikeButton");
   }
 
-*/
+  // Inserting new posts above the old ones
+  let previousPosts = document.getElementById("newPostsDiv");
+  previousPosts.insertBefore(divPost, votePost, previousPosts.firstChild);
 
-/*
-function upVote() {
-  let up = document.getElementsByTagName("h6");
-  for (let i = 0; i < up.length; i++) {
-  console.log(up[i].firstChild.data);
-} 
+  // Clearing fields after getting the values
+  document.getElementById("inputTitle").value = "";
+  document.getElementById("inputTagOne").value = "";
+  document.getElementById("inputTagTwo").value = "";
+  document.getElementById("inputTagThree").value = "";
+  document.getElementById("inputPost").value = "";
+
+
+  // Adding new posts to local storage
+  let checkStorage = localStorage.getItem("storingValues");
+
+  if (checkStorage != null) {
+
+    let storeValues = [postNewTitle, postNewTagOne, postNewTagTwo, postNewTagThree, postNewPost, postNewLikes]
+
+    let storingPosts = JSON.parse(localStorage.getItem("storingValues"));
+    storingPosts.unshift(storeValues);
+    localStorage.setItem("storingValues", JSON.stringify(storingPosts));
+  }
+
+  else {
+
+    let storeNewValues = [postNewTitle, postNewTagOne, postNewTagTwo, postNewTagThree, postNewPost, postNewLikes]
+    let storingPosts = [storeNewValues];
+    localStorage.setItem("storingValues", JSON.stringify(storingPosts));
+  }
+
 }
-*/
+postButton.addEventListener("click", onClick);
 
-
-/*
-function upVote() {
-  let up = document.getElementsByTagName("h6");
-  for (let i = 0; i < up.length; i++) {
-  console.log(up[i].firstChild.data);
-} 
-}*/
